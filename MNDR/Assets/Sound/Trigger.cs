08b86a2494374066f6af75261d04add30368cd5b
Elias Willerup <superdominoguy@gmail.com>
2022-10-07 19:57:16 -0600

    Spacial audio test scene
    Dev textures
    Test song
    Scene with 5 instruments

---
+++ MNDR/Assets/Sound/Trigger.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTriggerManager : MonoBehaviour {
+ 
+    public AudioClip nextMusic;
+    public AudioSource audioObject;
+ 
+    void Start()
+    {
+        audioObject = gameObject.GetComponent<AudioSource>();
+    }
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        { // only an object tagged Player stops the sound
+            audioObject.Stop();
+            Debug.Log("Player entered!");
+        }
+    }
+ 
+    void OnTriggerExit(Collider other)
+        //use ontriggerexit 2D instead of no 2D because of collider
+   
+    {
+        if (other.tag == "Player")
+        { // only an object tagged Player restarts the sound
+            audioObject.clip = nextMusic;  // select the next music
+            audioObject.Play(); // play it
+            Debug.Log("Player exit!");
+        }
+    }
+}
