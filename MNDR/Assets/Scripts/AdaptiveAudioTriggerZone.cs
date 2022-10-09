9f3a00fb8a33218790132e9b27fee0a048b53e0c
ewillerup <elias@willerup.com>
2022-10-09 14:37:07 -0600

    Adaptive audio
    New scene called Adaptive audio
    Orbiting audiosource
    Script that switches snapshots at a certain time

---
+++ MNDR/Assets/Scripts/AdaptiveAudioTriggerZone.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class AdaptiveAudioTriggerZone : MonoBehaviour {
+
+    public GameObject audioManager;
+
+    [Header("Snapshot to switch to upon player entering the trigger. (starts with 0)")]
+    public int snapshot;
+    [Header("Transition time to new snapshot.")]
+    public int transitionTime;
+
+    // Swap to an audio snapshot upon entering the trigger.
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player") {
+            audioManager.GetComponent<AdaptiveAudioManager>().SetAudioSnapshot(snapshot, transitionTime);
+            // Debug.Log("Switch audio");
+        }
+    }
+
+    // Swap to the default snapshot upon player exiting the trigger.
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player") {
+            audioManager.GetComponent<AdaptiveAudioManager>().SetDefaultSnapshot(transitionTime);
+        }
+    } 
+
+
+    // Draw unity editor trigger boxes a certain color, for organization
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = GetGizmoColor();
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, GetComponent<BoxCollider>().size);        
+    }  
+
+    private Color GetGizmoColor()
+    {
+        switch (snapshot)
+        {
+            case 0:
+            case 2:
+                return Color.black;  //default is black
+
+            case 1:
+                return Color.green;
+
+            case 3:
+                return Color.yellow;
+
+            case 4:
+                return new Color(.4f, .1f, .6f); //purple
+
+            case 5:
+                return Color.red;
+        }
+        return Color.black;
+    } 
+}
