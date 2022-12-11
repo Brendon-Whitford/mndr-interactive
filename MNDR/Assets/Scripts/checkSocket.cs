379db628451ff446d0ec14b37faaf9feb9ecbb0a
Redtropol <70288194+CameronsmithP@users.noreply.github.com>
2022-12-11 14:48:24 -0700

    pushing some slight adjustments to more inventory feats.

---
+++ MNDR/Assets/Scripts/checkSocket.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class checkSocket : MonoBehaviour
+{
+    public XRSocketInteractor socket;
+   
+
+  
+    void Start()
+    {
+        socket = GetComponent<XRSocketInteractor>();
+        
+        
+    }
+
+    // Update is called once per frame
+   public void socketCheck()
+    {
+        Debug.Log("ItemDetected");
+        
+       // GameObject item = socket.selectTarget.gameObject;
+       // DontDestroyOnLoad(item);
+       // Debug.Log(item);
+       
+       
+            
+            
+           
+        
+    }
+   
+}
