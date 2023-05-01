53b4497195f4e78f4a9112bd098d51b934559cb8
Javonmiller360 <javonmiller@u.boisestate.edu>
2023-04-30 23:29:20 -0600

    XR Rig and Inv fixed

--- MNDR/Assets/_Scripts/Card.cs
+++ MNDR/Assets/_Scripts/Card.cs
@@ -6,7 +6,6 @@ public class Card : MonoBehaviour
 {
     public bool collected = false;
     public GameObject button;
-    public IDscript idScript;
 
     // Start is called before the first frame update
     void Start()
