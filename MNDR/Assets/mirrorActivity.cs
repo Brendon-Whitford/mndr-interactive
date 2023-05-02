ab9e93907f8e262b045449088faccdc7d15ae846
jackslooney <70413420+jackslooney@users.noreply.github.com>
2023-05-01 18:06:47 -0600

    Multiple Activity Fixes
    Fixed interactions with the mirror and glasses activities in the cathedral scene.

--- MNDR/Assets/mirrorActivity.cs
+++ MNDR/Assets/mirrorActivity.cs
@@ -6,10 +6,13 @@ public class mirrorActivity : MonoBehaviour
 {
     public GenericInteractionActivity handler;
 
-
+    public bool activated = false;
     void OnTriggerEnter(Collider o) {
         if(o.tag == "Player") {
+            if(!activated) {
             handler.increment();
+            activated = true;
+            }
         }
     }
 }
