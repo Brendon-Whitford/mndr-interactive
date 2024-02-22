171b077828631e1a4c86a53a10a6181cf7b7b2fb
HaydenDton <haydendalton@u.boisestate.edu>
2024-02-21 23:31:53 -0700

    Added a spawn point script.
    Added spawning on a game object
    Invisible walls around the menu so plays can test walking around

--- MNDR/Assets/_Scripts/MainMenu/MainMenu.cs
+++ MNDR/Assets/_Scripts/MainMenu/MainMenu.cs
@@ -10,7 +10,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UI; 
 
 public class MainMenu : MonoBehaviour
 {
