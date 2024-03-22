431616bbee2f3f769d1bed1b92ac93d3c50d7c67
Carson McMahan <carson.mcmahan@gmail.com>
2024-03-21 18:53:57 -0700

    Created a Hover UI
    Created a reusable hover UI.

---
+++ MNDR/Assets/_Scripts/UI/UiLookAt.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Created By Carson McMahan || 03/21/2024
+ * This script is being used for UI that is shown while hovering over an object.
+ */
+
+public class UiLookAt : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    private void Awake()
+    {
+        playerTransform = GameObject.Find("XR Origin (1)").transform;
+    }
+
+    void Update()
+    {
+        // the transform of the object this is attached too will look at the player
+        transform.LookAt(playerTransform.position);
+    }
+}
