4a35a747cb0ff1fd9bd15810b3559dd00a7bcfba
Jacob Palin <jacobcpalin@gmail.com>
2024-03-26 13:37:26 -0600

    pause menu/how to play working

--- MNDR/Assets/_Scripts/MainMenu/PauseMenu.cs
+++ MNDR/Assets/_Scripts/MainMenu/PauseMenu.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XInput;
+using UnityEngine.SceneManagement;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.OpenXR.Input;
@@ -13,11 +15,27 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject playerPauseMenuSpawnPoint;
     [SerializeField] private bool menuActive;
     [SerializeField] private InputActionReference pauseMenuAction;
+    [SerializeField] private GameObject mainPage;
+    [SerializeField] private GameObject panel;
+    [SerializeField] private GameObject dinerButton;
+    [SerializeField] private UnityEngine.SceneManagement.Scene hubScene;
+    private static PauseMenu instance;
 
     private void Awake()
     {
+        hubScene = SceneManager.GetActiveScene();
         MenuVisibility(menuActive = false);
-        playerPauseMenuSpawnPoint = GameObject.FindGameObjectWithTag("Pause");
+        playerPauseMenuSpawnPoint = GameObject.FindGameObjectWithTag("Pause"); 
+        
+        DontDestroyOnLoad(this);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
     private void Update()
@@ -30,6 +48,15 @@ public class PauseMenu : MonoBehaviour
     {
         transform.position = playerPauseMenuSpawnPoint.transform.position;
         transform.rotation = playerPauseMenuSpawnPoint.transform.rotation;
+
+        if(SceneManager.GetActiveScene() != hubScene)
+        {
+            dinerButton.SetActive(true);
+        }
+        else if(SceneManager.GetActiveScene() == hubScene)
+        {
+            dinerButton.SetActive(false);
+        }
 
         if (menuActive == false)
         {
@@ -45,9 +72,7 @@ public class PauseMenu : MonoBehaviour
 
     private void MenuVisibility(bool visible)
     {
-        foreach (Transform child in gameObject.transform)
-        {
-            child.gameObject.SetActive(visible);
-        }
+        mainPage.SetActive(visible);
+        panel.SetActive(visible);
     }
 }
